934c6cebd764ab65285044ddf0f6b387e211da75
Bill <drbwho@hotmail.com>
2024-01-19 01:01:40 +0200

    dsfsd

--- src/app/providers/config-data.ts
+++ src/app/providers/config-data.ts
@@ -7,20 +7,21 @@ export class ConfigData {
 
   ENABLE_PUSH_NOTIFICATIONS = true;
 
-  API_URL = 'https://bkk-apps.com:8445';
+  //API_URL = 'https://bkk-apps.com:8445';
+  API_URL = 'https://precision-btc.eu/api/v1';
   CHAT_HOST = 'chat.bkk-apps.com';
 
   JSON_FILE = 'JSON_FILE';
-  API_JSONFILE_VERSION = this.API_URL + '/cod-mobile/json-version';
-  API_JSONFILE_URL = this.API_URL + '/sites/default/files/sessions.json';
+  API_JSONFILE_VERSION = this.API_URL + '/json-version';
+  API_JSONFILE_URL = this.API_URL + '/event/1';
 
   SESSION_RATINGS = 'SESSION_RATINGS';
   MY_SESSION_RATINGS = 'MY_SESSION_RATINGS';
   MY_SESSION_REVIEWS = 'MY_SESSION_REVIEWS';
-  API_GETRATINGS_URL = this.API_URL + '/cod-mobile/get-session-rating';
+  API_GETRATINGS_URL = this.API_URL + '/get-session-rating';
   API_SETRATING_URL = this.API_URL + '/cod-mobile/session-rating';
   API_SETREVIEWS_URL = this.API_URL + '/cod-mobile/session-reviews';
-  API_NEWS_URL =  this.API_URL + '/cod-mobile/get-news';
+  API_NEWS_URL =  this.API_URL + '/get-news';
   API_LOGIN_URL = this.API_URL + '/cod-mobile/user-authorization';
 
   NEWS_FILE = 'NEWS_FILE';
