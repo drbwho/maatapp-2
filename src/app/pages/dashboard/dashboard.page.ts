f82d6e2f15aaae1c2e7d7c3edcfe711dfd2ba824
Bill <darsinos@dhd.gr>
2026-02-05 10:13:27 +0200

    Sds

--- src/app/pages/dashboard/dashboard.page.ts
+++ src/app/pages/dashboard/dashboard.page.ts
@@ -3,6 +3,9 @@ import { DataProvider } from '../../providers/provider-data';
 import { NavController } from '@ionic/angular';
 import { Events } from '../../providers/events';
 import { ActivatedRoute } from '@angular/router';
+import { Storage } from '@ionic/storage-angular';
+import { ConfigData } from '../../providers/config-data';
+import { AppComponent } from '../../app.component';
 
 @Component({
   selector: 'app-dashboard',
@@ -15,13 +18,19 @@ export class DashboardPage implements OnInit {
   country = {id:"", name:"", currency:"", flagcode:"gb"};
   lastmeeting: any = {};
   account: any = {};
+  meeting_status = "";
+  upload_status = false;
+  network_status = false;
 
   constructor(
     private dataProvider: DataProvider,
     private navCtrl: NavController,
     private events: Events,
-    private route: ActivatedRoute
-  ) { 
+    private route: ActivatedRoute,
+    private storage: Storage,
+    private config: ConfigData,
+    private appcomponent: AppComponent
+  ) {
     this.route.url.subscribe(() => {
       this.load_currents(); // Hack to refresh page in every visit!
     });
@@ -44,6 +53,7 @@ export class DashboardPage implements OnInit {
       this.group = current.group;
       this.lastmeeting = current.group.lastmeeting;
       this.account = current.group.totals;
+      this.set_meeting_status();
     }
   }
 
@@ -54,6 +64,31 @@ export class DashboardPage implements OnInit {
     }else{
       this.navCtrl.navigateForward('/country/' + curcountry.id + '/groups');
     }
+  }
+
+  async set_meeting_status(){
+    this.network_status = this.appcomponent.networkStatus;
+    this.upload_status = await this.check_upload_status();
+    if(!this.lastmeeting){
+      this.meeting_status = "no-meetings";
+    }else if(!this.lastmeeting.endedat && !this.upload_status){
+      this.meeting_status = "in-progress";
+    }else if(this.lastmeeting.endedat && !this.upload_status){
+      this.meeting_status = "no-active";
+    }else if(!this.lastmeeting.endedat && this.upload_status && this.network_status){
+      this.meeting_status = "saved-local";
+    }else if(!this.lastmeeting.endedat && this.upload_status && !this.network_status){
+      this.meeting_status = "saved-offline";
+    }
+  }
+
+  async check_upload_status (){
+    var res = await this.storage.get(this.config.TRANSACTIONS_FILE);
+    var newmeetings = await this.storage.get(this.config.NEWMEETINS_FILE);
+    if((res && res.length) || newmeetings && newmeetings.length){
+      return true;
+    }
+    return false;
   }
 
 }
--- src/app/pages/tabs-page/tabs-page.ts
+++ src/app/pages/tabs-page/tabs-page.ts
@@ -1,7 +1,24 @@
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { AccountPage } from '../account/account';
 
 @Component({
     templateUrl: 'tabs-page.html',
     standalone: false
 })
-export class TabsPage {}
+export class TabsPage {
+
+  constructor(
+    private modalCtrl: ModalController
+  ){}
+
+  async openAccount(){
+    const modal = await this.modalCtrl.create({
+        component: AccountPage,
+        componentProps: { }
+      });
+      await modal.present();
+      await modal.onWillDismiss();
+  }
+
+}
