2c9cf27b5ade173e3c41f2aaa594df209e7d1cdf
unknown <drbwho@hotmail.com>
2026-03-02 10:07:31 +0200

    dsfsd

--- src/app/component/action-view/action-view.component.ts
+++ src/app/component/action-view/action-view.component.ts
@@ -2,11 +2,20 @@ import { Component, Input } from '@angular/core';
 import { IonicModule, ModalController } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 
+// needed fot translate pipe activation
+import { HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader();
+}
+
 @Component({
   selector: 'app-action-view',
   templateUrl: './action-view.component.html',
   styleUrls: ['./action-view.component.scss'],
-  imports: [IonicModule, CommonModule],
+  imports: [IonicModule, CommonModule, TranslateModule]
 })
 export class ActionViewComponent {
   @Input() title?: string;
--- src/app/pages/meetings/meetings.action-views.ts
+++ src/app/pages/meetings/meetings.action-views.ts
@@ -25,40 +25,35 @@ export class MeetingsActionViews {
   *
   */
   show_action_upload_close(meeting: any){
-    return new Promise((resolve)=>{
+    return new Promise(async (resolve)=>{
       if(!meeting){
         resolve(false);
         return;
       }
-      let keys = ['messages.meetings.upload-close.heading', 'messages.meetings.upload-close.description',
-        'messages.meetings.upload-close.button', 'messages.meetings.upload-close.button_1',
-        'data_uploaded','success','error'];
-
-      this.translate.get(keys).subscribe(async (keys)=>{
-        const modal = await this.modalCtrl.create({
-          component: ActionViewComponent,
-          componentProps: {
-            alttitle: meeting.place,
-            heading: keys['messages.meetings.upload-close.heading'],
-            description: keys['messages.meetings.upload-close.description'],
-            image: 'assets/img/action-views/upload-close-meeting.png',
-            hasBackButton: true,
-            buttons: [
-              {text: keys['messages.meetings.upload-close.button'], color: 'primary', action:'upload'},
-              {text: keys['messages.meetings.upload-close.button_1'], color: 'light', action:'close'},
-            ]
-          },
-          cssClass: ''
-        });
-        await modal.present();
-        await modal.onWillDismiss().then(async (data: any)=>{
-          if(data.data =='upload'){
-            resolve({success: await this.upload_meeting(meeting), action: 'upload'}); 
-          }
-          if(data.data =='close'){
-            resolve({success: await this.close_meeting(meeting), action: 'close'});  
-          }
-        });
+ 
+      const modal = await this.modalCtrl.create({
+        component: ActionViewComponent,
+        componentProps: {
+          alttitle: meeting.place,
+          heading: 'messages.meetings.upload-close.heading',
+          description: 'messages.meetings.upload-close.description',
+          image: 'assets/img/action-views/upload-close-meeting.png',
+          hasBackButton: true,
+          buttons: [
+            {text: 'messages.meetings.upload-close.button', color: 'primary', action:'upload'},
+            {text: 'messages.meetings.upload-close.button_1', color: 'light', action:'close'},
+          ]
+        },
+        cssClass: ''
+      });
+      await modal.present();
+      await modal.onWillDismiss().then(async (data: any)=>{
+        if(data.data =='upload'){
+          resolve({success: await this.upload_meeting(meeting), action: 'upload'}); 
+        }
+        if(data.data =='close'){
+          resolve({success: await this.close_meeting(meeting), action: 'close'});  
+        }
       });
     });
   }
@@ -90,126 +85,109 @@ export class MeetingsActionViews {
 
 
   show_action_upload_success(meeting: any){
-    return new Promise((resolve)=>{
+    return new Promise(async (resolve)=>{
       if(!meeting){
         resolve(false);
         return;
       }
-      let keys = ['messages.meetings.upload-success.heading', 'messages.meetings.upload-success.description',
-        'messages.meetings.upload-success.button', 'messages.meetings.upload-success.button_1',
-        'data_uploaded','success','error'];
 
-      this.translate.get(keys).subscribe(async (keys)=>{
-        let buttons = [];
-        if(!meeting.endedat){
-          buttons.push({text: keys['messages.meetings.upload-success.button'], color: 'primary', action:'close'});
+      let buttons = [];
+      if(!meeting.endedat){
+        buttons.push({text: 'messages.meetings.upload-success.button', color: 'primary', action:'close'});
+      }
+      buttons.push({text: 'messages.meetings.upload-success.button_1', color: 'light', action:'history'});
+
+      const modal = await this.modalCtrl.create({
+        component: ActionViewComponent,
+        componentProps: {
+          alttitle: meeting.place,
+          heading: 'messages.meetings.upload-success.heading',
+          description: 'messages.meetings.upload-success.description',
+          image: 'assets/img/action-views/upload-success-meeting.png',
+          hasBackButton: true,
+          buttons: buttons
+        },
+        cssClass: ''
+      });
+      await modal.present();
+      await modal.onWillDismiss().then(async (data: any)=>{
+        if(data.data =='close'){
+          resolve({success: await this.close_meeting(meeting), action: 'close'}); 
         }
-        buttons.push({text: keys['messages.meetings.upload-success.button_1'], color: 'light', action:'history'});
-
-        const modal = await this.modalCtrl.create({
-          component: ActionViewComponent,
-          componentProps: {
-            alttitle: meeting.place,
-            heading: keys['messages.meetings.upload-success.heading'],
-            description: keys['messages.meetings.upload-success.description'],
-            image: 'assets/img/action-views/upload-success-meeting.png',
-            hasBackButton: true,
-            buttons: buttons
-          },
-          cssClass: ''
-        });
-        await modal.present();
-        await modal.onWillDismiss().then(async (data: any)=>{
-          if(data.data =='close'){
-            resolve({success: await this.close_meeting(meeting), action: 'close'}); 
-          }
-          if(data.data =='history'){
-            resolve({action: 'history'});  
-          }
-        });
+        if(data.data =='history'){
+          resolve({action: 'history'});  
+        }
       });
     });
   }
 
   show_action_upload_failed(meeting: any){
-    return new Promise((resolve)=>{
+    return new Promise(async (resolve)=>{
       if(!meeting){
         resolve(false);
         return;
       }
-      let keys = ['messages.meetings.upload-failed.heading', 'messages.meetings.upload-failed.description',
-        'messages.meetings.upload-failed.button', 'messages.meetings.upload-failed.button_1',
-        'data_uploaded','success','error'];
-
-      this.translate.get(keys).subscribe(async (keys)=>{
-        const modal = await this.modalCtrl.create({
-          component: ActionViewComponent,
-          componentProps: {
-            alttitle: meeting.place,
-            heading: keys['messages.meetings.upload-failed.heading'],
-            description: keys['messages.meetings.upload-failed.description'],
-            image: 'assets/img/action-views/upload-success-meeting.png',
-            hasBackButton: true,
-            buttons: [
-              {text: keys['messages.meetings.upload-failed.button'], color: 'primary', action:'tryagain'},
-              {text: keys['messages.meetings.upload-failed.button_1'], color: 'light', action:'continue'}
-            ]
-          },
-          cssClass: ''
-        });
-        await modal.present();
-        await modal.onWillDismiss().then(async (data: any)=>{
-          resolve({action: data.data});
-        });
+      const modal = await this.modalCtrl.create({
+        component: ActionViewComponent,
+        componentProps: {
+          alttitle: meeting.place,
+          heading: 'messages.meetings.upload-failed.heading',
+          description: 'messages.meetings.upload-failed.description',
+          image: 'assets/img/action-views/upload-success-meeting.png',
+          hasBackButton: true,
+          buttons: [
+            {text: 'messages.meetings.upload-failed.button', color: 'primary', action:'tryagain'},
+            {text: 'messages.meetings.upload-failed.button_1', color: 'light', action:'continue'}
+          ]
+        },
+        cssClass: ''
+      });
+      await modal.present();
+      await modal.onWillDismiss().then(async (data: any)=>{
+        resolve({action: data.data});
       });
     });
   }
 
   show_action_cancel(meeting: any){
-    return new Promise((resolve)=>{
+    return new Promise(async (resolve)=>{
       if(!meeting){
         resolve(false);
         return;
       }
-      let keys = ['messages.meetings.cancel.heading', 'messages.meetings.cancel.description',
-        'messages.meetings.cancel.button', 'messages.meetings.cancel.button_1',
-        'success','error'];
-
-      this.translate.get(keys).subscribe(async (keys)=>{
-        const modal = await this.modalCtrl.create({
-          component: ActionViewComponent,
-          componentProps: {
-            alttitle: meeting.place,
-            heading: keys['messages.meetings.cancel.heading'],
-            description: keys['messages.meetings.cancel.description'],
-            image: 'assets/img/action-views/cancel-meeting.png',
-            hasBackButton: true,
-            buttons: [
-              {text: keys['messages.meetings.cancel.button'], color: 'primary', action:'cancel'},
-              {text: keys['messages.meetings.cancel.button_1'], color: 'light', action:'keep'}
-            ]
-          },
-          cssClass: ''
-        });
-        await modal.present();
-        await modal.onWillDismiss().then(async (data: any)=>{
-          if(data.data == 'cancel'){
-            // clear transactions
-            this.operationTools.clearPendingOperations(meeting).then((res)=>{
-              if(res){
-                // clear meeting
-                this.operationTools.clearPendingOperations(meeting, true).then((res)=>{
-                  resolve({success: res});
-                })
-              }else{
-                resolve({success: false});
-              }
-            })
-          }else{
-            resolve({success: false});
-          }
-        });
+      const modal = await this.modalCtrl.create({
+        component: ActionViewComponent,
+        componentProps: {
+          alttitle: meeting.place,
+          heading: 'messages.meetings.cancel.heading',
+          description: 'messages.meetings.cancel.description',
+          image: 'assets/img/action-views/cancel-meeting.png',
+          hasBackButton: true,
+          buttons: [
+            {text: 'messages.meetings.cancel.button', color: 'primary', action:'cancel'},
+            {text: 'messages.meetings.cancel.button_1', color: 'light', action:'keep'}
+          ]
+        },
+        cssClass: ''
       });
-    });    
+      await modal.present();
+      await modal.onWillDismiss().then(async (data: any)=>{
+        if(data.data == 'cancel'){
+          // clear transactions
+          this.operationTools.clearPendingOperations(meeting).then((res)=>{
+            if(res){
+              // clear meeting
+              this.operationTools.clearPendingOperations(meeting, true).then((res)=>{
+                resolve({success: res});
+              })
+            }else{
+              resolve({success: false});
+            }
+          })
+        }else{
+          resolve({success: false});
+        }
+      });
+    });   
   }
 }
